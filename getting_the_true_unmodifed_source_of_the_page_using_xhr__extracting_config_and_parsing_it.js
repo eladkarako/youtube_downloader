023c640d35ee7ec5c9b03df66415f24f5b5c60bb
Elad Karako <eladkarako@users.noreply.github.com>
2017-08-11 02:43:24 +0300

    including part of the chrome-extension, using xhr (and no cookies sending) the source of the page (NOT DOM!) can be retrieved, the config object renamed using text-based methods, and extracted - before the anti-spy cycles change/remove it. the new source can be injected to any page and the the object can be enumerated as usual...

---
+++ getting_the_true_unmodifed_source_of_the_page_using_xhr__extracting_config_and_parsing_it.js
@@ -0,0 +1,53 @@
+NodeList.prototype.filter = Array.prototype.filter;
+
+function get(url, done_callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    if(XMLHttpRequest.DONE !== xhr.readyState)            return;
+    if(null === xhr.response)                             return;
+    if("function" !== typeof xhr.response.querySelector)  return;
+    done_callback(xhr);
+  };
+  xhr.withCredentials = false;
+  xhr.responseType    = "document";
+  xhr.overrideMimeType("text/html;charset=UTF-8");
+  xhr.open('GET', url, true);
+  xhr.send();  
+}
+
+
+var REGEX = /ytplayer\.config\s*\=\s*\{/m;
+
+
+get(top.location.href, function(xhr){
+  var doc, ytplayerconfig_src;
+
+  doc = xhr.response;
+
+  ytplayerconfig_src = doc.querySelectorAll("script:not([src])")
+                          .filter(function(element){
+                            return REGEX.test(element.innerText)
+                          })
+                          .shift()
+                          ;
+  if("undefined" === typeof ytplayerconfig_src) return;
+  
+  //generate an alternative object
+  ytplayerconfig_src = ytplayerconfig_src.innerText
+                                         .replace(/[\r\n]+/g, " ")
+                                         .replace(/ytplayer/gm, "mockplayer")
+                                         ;
+
+  if("undefined" === typeof ytplayerconfig_src) return;
+
+  var script = document.createElement("script");  //main document.
+  script.innerHTML = ytplayerconfig_src;
+  (document.querySelector("body") || document.documentElement).appendChild(script);
+
+  if("undefined" === typeof mockplayer.config) return;
+
+  console.log(
+    self.mockplayer.config
+  )
+});
+
